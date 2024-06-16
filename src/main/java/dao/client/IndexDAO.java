510053141f93a1acb10a24bd112a1c6035d667fe
maithisuong2003 <21130516@st.hcmuaf.edu.vn>
2024-06-16 23:03:12 +0700

    fix loadMoreProduct using ajax

--- src/main/java/dao/client/IndexDAO.java
+++ src/main/java/dao/client/IndexDAO.java
@@ -68,25 +68,6 @@ public class IndexDAO {
         return list;
     }
 
-    public static List<Product> listRandProduct() {
-        List<Product> list = new ArrayList<>();
-        String query = "select * from Products  ORDER BY RAND() LIMIT 3;";
-        try {
-            Connection conn = JDBCUtil.getConnection();
-            PreparedStatement ps = conn.prepareStatement(query);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                list.add(new Product(rs.getInt(1), rs.getString(2), rs.getDouble(3), rs.getString(4), rs.getString(5),
-                        new Category(rs.getInt(6)))
-                );
-
-            }
-        } catch (Exception e) {
-
-        }
-        return list;
-
-    }
 
     public static List<Product> listRand4Product() {
         List<Product> list = new ArrayList<>();
@@ -107,62 +88,6 @@ public class IndexDAO {
 
     }
 
-	public static List<Product> getTop8() {
-		List<Product> list = new ArrayList<>();
-		String query = "SELECT * FROM Products ORDER BY price DESC LIMIT 8;";
-		try {
-			Connection conn =  JDBCUtil.getConnection();
-			PreparedStatement ps = conn.prepareStatement(query);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-                list.add( new Product(rs.getInt(1), rs.getString(2), rs.getDouble(3), rs.getString(4), rs.getString(5),
-                        new Category(rs.getInt(6))) );
-
-			}
-		} catch (Exception e) {
-
-		}
-		return list;
-	}
-public static List<Product> getOutstandingProduct() {
-	List<Product> list = new ArrayList<>();
-	String query = "SELECT * FROM Products ORDER BY price DESC LIMIT 4;";
-	try {
-		Connection conn = JDBCUtil.getConnection();
-		PreparedStatement ps = conn.prepareStatement(query);
-		ResultSet rs = ps.executeQuery();
-		while (rs.next()) {
-			list.add(new Product(rs.getInt(1), rs.getString(2), rs.getDouble(3), rs.getString(4), rs.getString(5),
-					new Category(rs.getInt(6))));
-		}
-
-
-	} catch (SQLException ex) {
-		throw new RuntimeException(ex);
-	}
-
-		return list;
-	}
-
-	public static List<Product> getNext4Product ( int amount){
-		List<Product> list = new ArrayList<>();
-		String query = "select * from Products where price>40 order by id limit 4 offset ?;";
-		try {
-			Connection con = JDBCUtil.getConnection();
-			PreparedStatement ps = con.prepareStatement(query);
-			ps.setInt(1, amount);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				list.add(new Product(rs.getInt(1), rs.getString(2), rs.getDouble(3), rs.getString(4), rs.getString(5),
-						new Category(rs.getInt(6))));
-
-			}
-		} catch (Exception e) {
-
-		}
-		return list;
-
-	}
 
 	public static List<Product> listRandProduct () {
 		List<Product> list = new ArrayList<>();
@@ -172,10 +97,9 @@ public static List<Product> getOutstandingProduct() {
 			PreparedStatement ps = conn.prepareStatement(query);
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
-				list.add( new Product(rs.getInt(1), rs.getString(2), rs.getDouble(3), rs.getString(4), rs.getString(5),
-						new Category(rs.getInt(6)),rs.getInt(7),rs.getDouble(8),rs.getDouble(9),AccountDAO.getAccountById(rs.getInt(1)),
-						new Provider(rs.getInt(1)),rs.getString(12),rs.getString(13))
-				);
+                list.add(new Product(rs.getInt(1), rs.getString(2), rs.getDouble(3), rs.getString(4), rs.getString(5),
+                        new Category(rs.getInt(6)))
+                );
 			}
 		} catch (Exception e) {
 
@@ -183,25 +107,7 @@ public static List<Product> getOutstandingProduct() {
 		return list;
 
 }
-	public static List<Product> listRand4Product () {
-		List<Product> list = new ArrayList<>();
-		String query = "select * from Products  ORDER BY RAND() LIMIT 4;";
-		try {
-			Connection conn = JDBCUtil.getConnection();
-			PreparedStatement ps = conn.prepareStatement(query);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				list.add(new Product(rs.getInt(1), rs.getString(2), rs.getDouble(3), rs.getString(4), rs.getString(5),
-						new Category(rs.getInt(6))));
 
-
-			}
-		} catch (Exception e) {
-
-		}
-		return list;
-
-}
 	public static void main(String[] args){
 		IndexDAO pd = new IndexDAO();
         System.out.println(getTop8());
